5b722badc3de9c4ba2c19017456c6cfe1cbc0986
Yongwen Zhuang <zyeoman@163.com>
2017-03-07 16:28:51 +0800

    :wrench: Update.sh more useful
    添加yn是否更新选项
    判断starred是否已安装

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-starred --username=zYeoman --sort > README.md
-git add .
-git commit -m ":sparkles: Update"
-git push
+if type "starred" > /dev/null; then
+    starred --username=zYeoman --sort > README.md
+else
+    echo "Please pip install starred first!"
+fi
+git diff
+read -p "Do you wish to commit and push?[Y/n]" yn
+case $yn in
+    [Nn]* ) exit;;
+    * ) git add . && git commit -m ":sparkles: Update" && git push;;
+esac
